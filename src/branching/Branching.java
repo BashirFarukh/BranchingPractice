7ce7ef03661f6e90f93af8a8ab6483d84ff37b9e
Bashir Farukh <bashir.farukh@tekschool.us>
2022-11-09 01:16:56 -0800

    Bashir

--- src/branching/Branching.java
+++ src/branching/Branching.java
@@ -3,8 +3,8 @@ package branching;
 public class Branching {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("This from Bashir");
 	}
 
 }
